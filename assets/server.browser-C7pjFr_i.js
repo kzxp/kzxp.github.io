c3c5d184a65be678d711059aace96669c387bde6
kzxp <kzxp@msn.com>
2024-12-17 17:06:56 +0000

    Deploying to master from @ kzxp/cv-generator@c84455c701299e0334d2be46e4b4302e9fb80939 🚀

--- assets/server.browser-C7pjFr_i.js
+++ assets/server.browser-C7pjFr_i.js
@@ -1,4 +1,4 @@
-import{r as pi,b as ji}from"./app-MFFTQzj_.js";var hr={};/**
+import{r as pi,b as ji}from"./app-6KTj_4me.js";var hr={};/**
  * @license React
  * react-dom-server-legacy.browser.production.js
  *
